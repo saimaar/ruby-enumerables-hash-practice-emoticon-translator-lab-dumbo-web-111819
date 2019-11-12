bbe56434b326943b80b0bb0d9341915a2a534f8e
Saima Rahman <rahman.saima92@gmail.com>
2019-11-12 19:14:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,8 @@ require 'yaml'
 
 def load_library(file_path)
  emoticons = YAML.load_file(file_path)
- new_hash ={ :get_meaning => nil, :get_emoticon => nil}
+ new_hash ={ :get_meaning => nil, 
+            :get_emoticon => nil}
 end
 
 def get_japanese_emoticon
