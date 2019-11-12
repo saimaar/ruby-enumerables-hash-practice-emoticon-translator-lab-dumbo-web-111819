a7f77f9096739d35f4d93f1e0232de60729f194a
Saima Rahman <rahman.saima92@gmail.com>
2019-11-12 19:58:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,6 +31,6 @@ def get_japanese_emoticon(file_path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
 end
